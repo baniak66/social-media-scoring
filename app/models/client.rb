e207699338e20aa17baceedeb02bdbbe001c3f45
Pawel Licznar <pawellicznar@gmail.com>
2017-05-21 00:27:49 +0200

    Count scores depend on type

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,7 +2,6 @@ class Client < ApplicationRecord
   has_many :likes
 
   def check_likes
-    start = 0
     clients_likes = []
     self.likes.each { |like| clients_likes << like.content }
     important_likes = []
@@ -10,4 +9,23 @@ class Client < ApplicationRecord
     clients_likes & important_likes
   end
 
+  def score(type)
+    scores = []
+    check_likes.each do |like|
+      ImportantLike.where(content: like).each do |l|
+        if type == "fin"
+          scores << l.fin
+        elsif type == "mat"
+          scores << l.mat
+        elsif type == "gro"
+          scores << l.gro
+        else type == "hip"
+          scores << l.hip
+        end
+      end
+    end
+    scores.reduce(:+)
+  end
+
 end
+
