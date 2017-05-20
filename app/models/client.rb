9ab81058344b5123790e915254e9319fc624401a
Pawel Licznar <pawellicznar@gmail.com>
2017-05-20 23:48:24 +0200

    Check important clients links

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,13 @@
 class Client < ApplicationRecord
   has_many :likes
 
+  def check_likes
+    start = 0
+    clients_likes = []
+    self.likes.each { |like| clients_likes << like.content }
+    important_likes = []
+    ImportantLike.all.each { |like| important_likes << like.content }
+    clients_likes & important_likes
+  end
 
 end
